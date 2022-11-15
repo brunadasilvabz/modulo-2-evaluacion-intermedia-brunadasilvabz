32e48e298d5ea800d98ca0d8753fa44eb0735014
Bruna <brunadasilvabz98@gmail.com>
2022-11-15 18:43:33 +0100

    fix javascript

--- js/main.js
+++ js/main.js
@@ -1,114 +1,59 @@
-'use strict';
+"use strict";
 
-const select = document.querySelector('.js-select');
-const btn = document.querySelector('.js-btn');
-const text = document.querySelector('.js-text');
+//-------------------QUERYSELECTORS-------------------
 
-let computerChoice;
-let textResult;
+const selectElement = document.querySelector(".js-select");
+const btn = document.querySelector(".js-btn");
+const resultText = document.querySelector(".js-text");
 
-
-
-//mi planteamiento es que cada raza buena y cada raza mala están en posiciones determinadas y al elegir se compara para saber cuál es más fuerte (pero es de manera manual, así que sé que no es la mejor idea porque es mucho código)
-
+//-------------------FUNCIONES-------------------
 
 function getRandomNumber(max) {
-    return Math.ceil(Math.random() * max);
+  //función que nos dan para calcular
+  return Math.ceil(Math.random() * max);
 }
 
-function computerRandom() {
-    const randomNumber = getRandomNumber(6);
-    if(randomNumber === 1) {
-        computerChoice = 'race1'
-    }
-    if(randomNumber === 2) {
-        computerChoice = 'race2'
-    }
-    if(randomNumber === 3) {
-        computerChoice = 'race3'
-    }
-    if(randomNumber === 4) {
-        computerChoice = 'race4'
-    }
-    if(randomNumber === 5) {
-        computerChoice = 'race5'
-    }
-    console.log(computerChoice);
-    
+function getComputerChoice() {
+  //función para conseguir un número random según las razas malas. Mi planteamiento anterior estaba mal!
+  const randomNumber = getRandomNumber(5);
+
+  let result = 0;
+
+  if (randomNumber === 1 || randomNumber === 2 || randomNumber === 3) {
+    result = 2;
+  } else if (randomNumber === 4) {
+    result = 3;
+  } else {
+    result = 5;
+  }
+  return result;
 }
 
-/*function userChoice() {
-    const selectValue = select.value;
-    console.log (selectValue);
-}*/
-
-
-function result() { 
-    const selectValue = select.value;
-
-    //option 1
-    if (computerChoice === 'race1' && selectValue === 'race1') {
-        textResult = 'Ha ganado el Ejército del Mal! Vuelve a intentarlo.';
-    }
-    if (computerChoice === 'race1' && selectValue === 'race2') {
-        textResult = 'Empate';
-    }
-    if (computerChoice === 'race1' && selectValue === 'race3') {
-        textResult = 'Ha ganado el Ejército del Bien! Enhorabuena.';
-    }
-    if (computerChoice === 'race1' && selectValue === 'race4') {
-        textResult = 'Ha ganado el Ejército del Bien! Enhorabuena.';
-    }
-    if (computerChoice === 'race1' && selectValue === 'race5') {
-        textResult = 'Ha ganado el Ejército del Bien! Enhorabuena.';
-    }
-    
-    //option2
-    if (computerChoice === 'race2' && selectValue === 'race1') {
-        textResult = 'Ha ganado el Ejército del Mal! Vuelve a intentarlo.';
-    }
-    if (computerChoice === 'race2' && selectValue === 'race2') {
-        textResult = 'Empate';
-    }
-    if (computerChoice === 'race2' && selectValue === 'race3') {
-        textResult = 'Ha ganado el Ejército del Bien! Enhorabuena.';
-    }
-    if (computerChoice === 'race2' && selectValue === 'race4') {
-        textResult = 'Ha ganado el Ejército del Bien! Enhorabuena.';
-    }
-    if (computerChoice === 'race2' && selectValue === 'race5') {
-        textResult = 'Ha ganado el Ejército del Bien! Enhorabuena.';
-    }
-
-    //option3
-    if (computerChoice === 'race3' && selectValue === 'race1') {
-        textResult = 'Ha ganado el Ejército del Mal! Vuelve a intentarlo.';
-    }
-    if (computerChoice === 'race3' && selectValue === 'race2') {
-        textResult = 'Empate';
-    }
-    if (computerChoice === 'race3' && selectValue === 'race3') {
-        textResult = 'Ha ganado el Ejército del Bien! Enhorabuena.';
-    }
-    if (computerChoice === 'race3' && selectValue === 'race4') {
-        textResult = 'Ha ganado el Ejército del Bien! Enhorabuena.';
-    }
-    if (computerChoice === 'race3' && selectValue === 'race5') {
-        textResult = 'Ha ganado el Ejército del Bien! Enhorabuena.';
-    }
-
-    //option4
-    //option5
-
-    text.innerHTML = textResult;
+function getUserChoice() {
+  //función para conseguir la raza que quiere la usuaria
+  return parseInt(selectElement.value);
 }
 
-
+function getResult(userChoice, computerChoice) {
+  //función para comparar y recibir un resultado
+  if (userChoice > computerChoice) {
+    resultText.innerHTML = "¡Ha ganado el Ejército del Bien! Enhorabuena.";
+  } else if (userChoice < computerChoice) {
+    resultText.innerHTML =
+      "Ha ganado el Ejército del Mal! Vuelve a intentarlo.";
+  } else {
+    resultText.innerHTML = "Empate";
+  }
+}
 
 function handleClick(event) {
-    event.preventDefault();
-    computerRandom();
-    result();
+  //función manejadora con todo lo que debe ocurrir al hacer click en el botón
+  event.preventDefault();
+  const computerChoice = getComputerChoice();
+  const userChoice = getUserChoice();
+  getResult(userChoice, computerChoice);
 }
 
-btn.addEventListener('click', handleClick);
+//-------------------EVENTOS-------------------
+
+btn.addEventListener("click", handleClick);
